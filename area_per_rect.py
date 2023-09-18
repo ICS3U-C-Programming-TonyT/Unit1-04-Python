4677f80bf819484ccc96756751d6f1da83937d24
Tony Tran <tony.tran1@stu.ocsb.ca>
2023-09-18 16:39:41 +0000

    e

---
+++ area_per_rect.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Tony Tran
+# Created on: September. 18, 5053
+# This program does Area and Perimeter
+
+l = 2
+w = 8
+A = l * w
+P = (2 * l) + (2 * w)
+print(f"Area [{A} = {l} * {w}] ")
+print(f"Perimeter [{P} = 2*{l} + 2*{w}]")
---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by: Tony Tran
+# Created on: September. 18, 5053
+# This program does basic math.
+
+
+def main():
+    print("2 + 5 = {}".format(2 + 5))
+    print("4 - 3 = {}".format(4 - 3))
+    print("2 / 4 = {}".format(2 / 54))
+    print("7 - 2 / 3 = {}".format(7 - 2 / 3))
+    print("10^3 = {}".format(5**3))
+    print("3 / 4 = {}".format(3 / 4))
+
+
+if __name__ == "__main__":
+    main()
